2e9ddb4c72bfa192f86d4a60cbddf5454058259d
Abdur Razzak <abdurrazzakyeamin@gmail.com>
2024-09-22 23:10:04 +0600

    login page and validation added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 document.getElementById('loginBtn').addEventListener('click', function (event) {
     event.preventDefault();
     const phoneNumber = document.getElementById('phoneNumber').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pinNumber').value;
+    if (phoneNumber === '01645915231' && pinNumber === '1234') {
+        console.log('Loged in');
+        window.location.href = "/home.html"
+    }
+    else {
+        alert('Wrong Number or Password');
+    }
+    console.log(phoneNumber,pinNumber);
     
 })
