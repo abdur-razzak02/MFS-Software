170fafa16be0cd541cab70d5c089c69352e5e26b
Abdur Razzak <abdurrazzakyeamin@gmail.com>
2024-09-22 14:26:53 +0600

    added to github

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('loginBtn').addEventListener('click', function (event) {
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    console.log(phoneNumber);
+    
+})
